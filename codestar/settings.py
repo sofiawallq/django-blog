0714cceb12f30d77347e86eb369d96a58cbacbbf
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-08-05 12:33:05 +0000

    add function for opening blog post

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-sofiawallq-djangoblog-6pgzdaz8ptf.ws.codeinstitute-ide.net', 
                 '.herokuapp.com']
