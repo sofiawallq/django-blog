d6ff1001ad15ec7a7cedea1470a39323be941920
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-08-08 11:39:15 +0000

    add authentication, register and login

--- about/views.py
+++ about/views.py
@@ -7,7 +7,7 @@ def about_me(request):
     """
     Renders the About page
     """
-    about = About.objects.all().order_by('-updated_on').first()
+    about = About.objects.all().order_by('-created_on').first()
 
     return render(
         request,
